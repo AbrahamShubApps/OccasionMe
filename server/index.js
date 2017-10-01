7a52e5165d434bf9eadc9ad903519d52ee5a279b
Menachem Abraham <menachemabraham@gmail.com>
2017-10-01 16:29:17 -0400

    Include static page with web-app bundle

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const eventsRouter = require('./routers/events');
 
 const app = express();
 
+app.use(express.static(`${__dirname}/../web_app/dist`));
+
 app.use('/events/', eventsRouter);
 
 app.listen(3000, () => {
--- web_app/webpack.config.js
+++ web_app/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './app.jsx',
   output: {
     path: path.resolve(__dirname),
-    filename: './bundle.js'
+    filename: './dist/bundle.js'
   },
   module: {
     loaders: [
