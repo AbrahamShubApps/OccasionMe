cafd63ec382e37f809e09868029d45164f3d94a0
Menachem Abraham <menachemabraham@gmail.com>
2017-10-08 23:25:38 -0400

    Add user infrustructure

--- web_app/components/app.jsx
+++ web_app/components/app.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { signup } from '../util/session_util';
 
 const App = props => {
+  signup('username@whatever.com', 'mypassword')
+    .then(res => console.log(res));
   return (
     <div>
-      Hello World!
+      App goes here!!!
     </div>
   )
 }
