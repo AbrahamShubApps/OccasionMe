2d749c8456cb3c0ff1f782fce1eac4b2c8f4d47a
waltershub <waltershub@gmail.com>
2017-10-18 00:14:42 -0400

    started working on redux

---
+++ mobile/config/congif.js
@@ -0,0 +1,22 @@
+const config = {
+  firebase: {
+    apiKey: 'AIzaSyDMLN3HRDYR155D1QvD6yYOkxnkw08WXA4',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+  },
+  facebook: {
+    API_KEY: '',
+  },
+  aws: {
+    accessKey: '',
+    secretKey: '',
+  },
+  google: {
+    key: '',
+  },
+};
+
+export default config;
