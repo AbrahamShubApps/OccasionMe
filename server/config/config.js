8795b2933f826f823d82ae58424a68fe5ac47184
Menachem Abraham <menachemabraham@gmail.com>
2018-01-21 21:57:37 -0500

    Add database_url to sequelize config

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
-{
+export default {
   "development": {
     "username": "menachem",
     "password": null,
@@ -14,6 +14,7 @@
     "dialect": "postgres"
   },
   "production": {
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "host": process.env.DATABASE_URL
   }
 }
